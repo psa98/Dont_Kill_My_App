23a5cb59b82d850a0a7787eae6e250c2c9f59f6f
ponom <461300@mail.ru>
2021-08-10 23:44:06 +0300

    V 1.1
    Осталось только доки написать на английском
    
    Логгер пропущенных выставлен на 240 секунд, время таймера на 15 секунд
    
    добавлен интерфейс и слушатель, протестирована отписка от событий
    
    Добавлен модуль - библиотека без логов и всего лишнего, и русская документация

--- app/keep_alive_library/src/main/java/c/ponom/keep_alive_library/BackgroundWorker.java
+++ app/keep_alive_library/src/main/java/c/ponom/keep_alive_library/BackgroundWorker.java
@@ -8,11 +8,11 @@ public class BackgroundWorker {
      *  тут можно, к примеру, инициировать для последующего обзора через  observeForever лайфдаты
      *  полученные через lifeKeeper.subscribeOn..., или выполнить другие однократные действия
      *  и реализовать все другие обработчики
-     * метод к примеру можно вызвать  при автозапуске/перезапуске если в onCreate Application класса
-     * есть следующий код:
-     * LifeKeeper.getInstance().start(this);
-     * MyBackgroundWork myBackGroundWork = new MyBackgroundWork();
-     * myBackGroundWork.backgroundProcessorSetup();
+     * метод  можно вызвать  при автозапуске/перезапуске если в onCreate Application класса
+     * есть следующий код:<BR>
+     * LifeKeeper.getInstance().start(this);<BR>
+     * MyBackgroundWork myBackGroundWork = new MyBackgroundWork();<BR>
+     * myBackGroundWork.backgroundProcessorSetup(); <BR>
      * Фактически данный класс  используется для разгрузки Application от излишнего стартового  кода
      */
     public void backgroundProcessorSetup() {
